cb29a7e3d42c6b39cef48f2f47405d3825333402
Syd270 <165206532+Syd270@users.noreply.github.com>
2024-06-17 22:29:15 -0600

    Colour matched with rest of the screens

--- CMPT291DB/ReportsMain.Designer.cs
+++ CMPT291DB/ReportsMain.Designer.cs
@@ -39,74 +39,100 @@
             // 
             // dataGridViewReports
             // 
+            dataGridViewReports.BackgroundColor = Color.White;
             dataGridViewReports.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewReports.Location = new Point(12, 12);
+            dataGridViewReports.Location = new Point(10, 9);
+            dataGridViewReports.Margin = new Padding(3, 2, 3, 2);
             dataGridViewReports.Name = "dataGridViewReports";
             dataGridViewReports.RowHeadersWidth = 51;
-            dataGridViewReports.Size = new Size(524, 255);
+            dataGridViewReports.Size = new Size(458, 191);
             dataGridViewReports.TabIndex = 0;
             // 
             // buttonReport1
             // 
-            buttonReport1.Location = new Point(12, 290);
+            buttonReport1.BackColor = Color.FromArgb(39, 157, 86);
+            buttonReport1.ForeColor = Color.White;
+            buttonReport1.Location = new Point(10, 218);
+            buttonReport1.Margin = new Padding(3, 2, 3, 2);
             buttonReport1.Name = "buttonReport1";
-            buttonReport1.Size = new Size(100, 40);
+            buttonReport1.Size = new Size(88, 30);
             buttonReport1.TabIndex = 1;
             buttonReport1.Text = "Report 1";
-            buttonReport1.UseVisualStyleBackColor = true;
+            buttonReport1.UseVisualStyleBackColor = false;
+            buttonReport1.Click += buttonReport1_Click;
             // 
             // buttonReport2
             // 
-            buttonReport2.Location = new Point(118, 290);
+            buttonReport2.BackColor = Color.FromArgb(39, 157, 86);
+            buttonReport2.ForeColor = Color.White;
+            buttonReport2.Location = new Point(103, 218);
+            buttonReport2.Margin = new Padding(3, 2, 3, 2);
             buttonReport2.Name = "buttonReport2";
-            buttonReport2.Size = new Size(100, 40);
+            buttonReport2.Size = new Size(88, 30);
             buttonReport2.TabIndex = 2;
             buttonReport2.Text = "Report 2";
-            buttonReport2.UseVisualStyleBackColor = true;
+            buttonReport2.UseVisualStyleBackColor = false;
+            buttonReport2.Click += buttonReport2_Click;
             // 
             // buttonReport3
             // 
-            buttonReport3.Location = new Point(224, 290);
+            buttonReport3.BackColor = Color.FromArgb(39, 157, 86);
+            buttonReport3.ForeColor = Color.White;
+            buttonReport3.Location = new Point(196, 218);
+            buttonReport3.Margin = new Padding(3, 2, 3, 2);
             buttonReport3.Name = "buttonReport3";
-            buttonReport3.Size = new Size(100, 40);
+            buttonReport3.Size = new Size(88, 30);
             buttonReport3.TabIndex = 3;
             buttonReport3.Text = "Report 3";
-            buttonReport3.UseVisualStyleBackColor = true;
+            buttonReport3.UseVisualStyleBackColor = false;
+            buttonReport3.Click += buttonReport3_Click;
             // 
             // buttonReport4
             // 
-            buttonReport4.Location = new Point(330, 290);
+            buttonReport4.BackColor = Color.FromArgb(39, 157, 86);
+            buttonReport4.ForeColor = Color.White;
+            buttonReport4.Location = new Point(289, 218);
+            buttonReport4.Margin = new Padding(3, 2, 3, 2);
             buttonReport4.Name = "buttonReport4";
-            buttonReport4.Size = new Size(100, 40);
+            buttonReport4.Size = new Size(88, 30);
             buttonReport4.TabIndex = 4;
             buttonReport4.Text = "Report 4";
-            buttonReport4.UseVisualStyleBackColor = true;
+            buttonReport4.UseVisualStyleBackColor = false;
+            buttonReport4.Click += buttonReport4_Click;
             // 
             // buttonReport5
             // 
-            buttonReport5.Location = new Point(436, 290);
+            buttonReport5.BackColor = Color.FromArgb(39, 157, 86);
+            buttonReport5.ForeColor = Color.White;
+            buttonReport5.Location = new Point(382, 218);
+            buttonReport5.Margin = new Padding(3, 2, 3, 2);
             buttonReport5.Name = "buttonReport5";
-            buttonReport5.Size = new Size(100, 40);
+            buttonReport5.Size = new Size(88, 30);
             buttonReport5.TabIndex = 5;
             buttonReport5.Text = "Report 5";
-            buttonReport5.UseVisualStyleBackColor = true;
+            buttonReport5.UseVisualStyleBackColor = false;
+            buttonReport5.Click += buttonReport5_Click;
+            // 
+           
             // 
             // ReportsMain
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(51, 54, 63);
-            ClientSize = new Size(548, 342);
+            ClientSize = new Size(480, 256);
             Controls.Add(buttonReport5);
             Controls.Add(buttonReport4);
             Controls.Add(buttonReport3);
             Controls.Add(buttonReport2);
             Controls.Add(buttonReport1);
             Controls.Add(dataGridViewReports);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "ReportsMain";
             Text = "ReportsMain";
             ((System.ComponentModel.ISupportInitialize)dataGridViewReports).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -117,5 +143,6 @@
         private Button buttonReport3;
         private Button buttonReport4;
         private Button buttonReport5;
+        
     }
 }
